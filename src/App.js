bfc382ba4c1c574e11831c45ea00992f1841a907
Santhoshkumar Rajannan <muruganandam.vg@nulogic.io>
2024-03-22 15:26:29 +0530

    initial setup for rhk and comparison with traditional react form

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import { useState } from 'react';
+import FormRHF from './components/FormRHF';
+import NormalForm from './components/NormalForm';
+import MUIComponent from './components/materialui/MUIComponent';
+import Form from './components/integration/Form';
+import HookForm from './components/reusuable/HookForm';
+function App() {
+  return (
+    <div >
+      {/* <FormRHF /> */}
+      <NormalForm/>
+      {/* <MUIComponent/> */}
+      {/* <Form /> */}
+      {/* <HookForm/> */}
+
+    </div>
+  )
+
+}
+  
+export default App;
